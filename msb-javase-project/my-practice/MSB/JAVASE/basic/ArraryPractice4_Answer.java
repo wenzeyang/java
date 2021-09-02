37abcb89eb1a23933aade435e92d2a1d10da65c5
vic <vic.young.2012@gmail.com>
2021-09-02 09:36:03 +0800

    add 2021/9/1

--- msb-javase-project/my-practice/MSB/JAVASE/basic/ArraryPractice4_Answer.java
+++ msb-javase-project/my-practice/MSB/JAVASE/basic/ArraryPractice4_Answer.java
@@ -9,6 +9,25 @@ package basic;
  */
 public class ArraryPractice4_Answer {
     public static void main(String[] args) {
-        
+        int[][] arr= new int[10][10];
+        //
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print(arr[i][j]+"\t");
+//            }
+//            System.out.println();
+//        }
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i){
+                    arr[i][j]=1;
+                }else{
+                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                }
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
     }
 }
